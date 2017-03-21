55430757bb70dcb704ec0a1ff75625f712479d65
Dwane <Dwane@hiphopandcode.com>
2017-03-21 18:35:08 -0400

    basic working shopping cart

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function updateCart(){
       <div>`+shoppingCart[i][0]+`</div>
       <div>`+shoppingCart[i][3]+`</div>
       <div>`+shoppingCart[i][2]+`</div>
-      <div>`+shoppingCart[i][1]+`</div>
+      <div>$`+shoppingCart[i][1]+`</div>
     </div>
     `
     list.innerHTML = list.innerHTML + productInfo;
@@ -47,27 +47,26 @@ let shirtSizes = document.querySelectorAll('.shirtSize');
 let shirtTitles = document.querySelectorAll('.title');
 let shirtPrices = document.querySelectorAll('.price');
 let shirtColors = document.querySelectorAll('.colorSelected');
-console.log(storeItems);
+//console.log(storeItems);
 for (let i = 0; i < storeItems.length; i++) {
   let cartButton = cartButtons[i];
   let shirtSize = shirtSizes[i].textContent;
   let shirtTitle = shirtTitles[i].textContent;
   let shirtPrice = parseInt(shirtPrices[i].textContent);
   let shirtColor = shirtColors[i].classList[2];
-  console.log(shirtColor);
+  //console.log(shirtColor);
   productHolder[i] = [shirtTitle,shirtPrice,shirtSize,shirtColor];
   cartButton.addEventListener('click', function () {
     productHolder[i][2] = shirtSizes[i].textContent;
     productHolder[i][3] = shirtColors[i].classList[2];
-    console.log("shopping cart clicked! store item: "+ i)
-    console.log(productHolder[i]);
-    shoppingCart[shoppingCart.length] = productHolder[i];
-    console.log(shoppingCart);
-
+    //console.log("shopping cart clicked! store item: "+ i)
+    //console.log(productHolder[i]);
+    shoppingCart[shoppingCart.length] = [shirtTitle,shirtPrice,shirtSizes[i].textContent,shirtColors[i].classList[2]];
+    //console.log(shoppingCart);
     shoppingCartTotal = shoppingCartTotal + productHolder[i][1];
     let element = document.querySelector("#shoppingCartTotal");
     element.textContent = shoppingCartTotal;
-    console.log(shoppingCartTotal);
+    //console.log(shoppingCartTotal);
     updateCart();
   });
 }
